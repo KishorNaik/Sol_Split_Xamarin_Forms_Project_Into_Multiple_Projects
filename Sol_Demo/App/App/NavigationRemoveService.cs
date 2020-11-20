dbae3b30f9b18cb9d965be8e54d505a191fafd12
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-20 12:50:43 +0530

    Code Commit

---
+++ Sol_Demo/App/App/NavigationRemoveService.cs
@@ -0,0 +1,45 @@
+﻿using NavigationServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using App.Views;
+using CircleImageDemo.Views;
+using CardViewDemo.Views;
+
+namespace App
+{
+    public class NavigationRemoveService : INavigationRemoveService
+    {
+        private Task RemovePages(Type type)
+        {
+            return Task.Run(() =>
+            {
+                var existingPages = App.Current.MainPage.Navigation.NavigationStack.ToList();
+
+                foreach (var page in existingPages)
+                {
+                    if (page.GetType() == type)
+                        continue;
+                    App.Current.MainPage.Navigation.RemovePage(page);
+                }
+            });
+        }
+
+        async Task INavigationRemoveService.RemoveOthersPagesExceptAnimalViewPageNavigationStack()
+        {
+            await this.RemovePages(typeof(AnimalViewPage));
+        }
+
+        async Task INavigationRemoveService.RemoveOthersPagesExceptCardViewPageNavigationSatck()
+        {
+            await this.RemovePages(typeof(CardViewPage));
+        }
+
+        async Task INavigationRemoveService.RemoveOthersPagesExceptMainPageNavigationStack()
+        {
+            await this.RemovePages(typeof(MainPage));
+        }
+    }
+}
